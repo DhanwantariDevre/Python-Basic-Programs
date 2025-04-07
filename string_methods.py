052a036dcbdc479b5d64dbdc52ae8b2acece6c34
Dhanwantari Devre <103302150+DhanwantariDevre@users.noreply.github.com>
2025-04-07 19:43:37 +0530

    Add files via upload

---
+++ string_methods.py
@@ -0,0 +1,113 @@
+
+#String is a set of characters
+
+'''str1="Hello "
+print(str1)
+str2="World"
+print(str2)
+
+#String concatenation using + operator
+str3=str1+str2
+print(str3)
+
+#To print the string multiple times we have to use * operator
+str4=str1*3
+print(str4)'''
+
+#Methods of string
+#1.lower() :It converts all uppercase characters in a string into lowercase
+str1="Hello World"
+print(str1)
+print(str1.lower())
+
+#2.upper()
+str1="Hello World"
+print(str1)
+print(str1.upper())
+
+#3.title() : Converts the string into title case
+str2="welcome to the python class "
+print(str2.title())
+
+
+
+
+print(str1.lower()) #it converts the string into lower case
+print(str1.upper()) # it converts the string into upper case
+
+#title()
+print(str1.title()) #It coverts the first letter of each word in uppercase
+
+str1= "helloWorldisthebasicstatement"
+str2="World23"
+str3="123 45"
+
+#isalpha()
+#print(str1.isalpha()) #It must requires all the alphabets in a string
+#print(str2.isalpha())
+#If there is a single space in the string of digit then it will answer as FALSE
+
+
+#isdigit()
+print(str1.isdigit()) #It must requires all the digits in a string
+print(str2.isdigit())
+print(str3.isdigit())
+#If there is a single space in the string of digit then it will answer as FALSE
+
+
+str1= "hello World is the basic statement"
+str2="World23"
+str3="123 45"
+#isalnum()
+print(str2.isalnum())
+
+#swapcase()
+print(str1.swapcase()) #it will simply swap the cases of the string
+
+capitalize()
+str_cap = str1.capitalize() # it will converts the first letter of the string into uppercase
+print(str_cap)
+
+str1= "hello World is the is is basic is statement"
+str2="World23"
+str3="123 45"
+
+#endswith()
+#print(str1.endswith("hello"))
+
+#startswith()
+#print(str1.startswith("hello"))
+
+#count()
+#print(str1.count("is"))
+
+#index()
+#print(str1.index("is")) #It returns the starting index of the substring we passed
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
